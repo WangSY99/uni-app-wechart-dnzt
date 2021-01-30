026aae426c56f7bfbf79d2df9aee693443fb83d1
jake99 <1427170192@qq.com>
2021-01-30 23:59:36 +0800

    懂你找图第一次提交

---
+++ src/pages/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>首页
+  <uni-badge text="1"></uni-badge>
+<uni-badge text="2" type="success" @click="bindClick"></uni-badge>
+<uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+  </view>
+</template>
+
+<script>
+import {uniBadge} from '@dcloudio/uni-ui'
+export default {
+components: {uniBadge}
+
+};
+</script>
+
+<style>
+
+</style>
